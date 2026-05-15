3942c791762ec22b9dc26218ab659003e3df0a06
Kaizzendev <Kaizzendev@gmail.com>
2026-05-15 20:56:39 +0200

    feat(basic structure)

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <div> 
+            About Page
+
+            <p>I started in web/backend development and QA, 
+                and transitioned into game development to specialize in gameplay systems and procedural design. 
+                I enjoy building systemic games with replayability and emergent behavior.
+            </p>
+        </div>
+
+    );
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import GameCard from "./game_card_component";
+
+export default function Portfolio() {
+    return (
+        <div> 
+            Portfolio Page
+            <li>
+                <GameCard />
+            </li>
+        </div>
+
+    );
+}
